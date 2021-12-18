b8bec7a2a2d5e4823bb5ad507e0fdfcfac6d700c
RAADAR <57947405+Raadar101@users.noreply.github.com>
2021-12-18 11:49:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const person = {
+    firstName: "Ali",
+    LastName: "Musa",
+    age: 21,
+    title: 'student',
+    friends: ["Sahra", 'Ahmed', "Asho", "Adam"]
+};
+console.log(person.LastName);
